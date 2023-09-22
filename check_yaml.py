6670c425f69da310400affd228ccdc6ae7560cfa
unknown <AAnderson@spartaglobal.com>
2023-09-22 12:25:54 +0100

    first commit

---
+++ check_yaml.py
@@ -0,0 +1,16 @@
+import os
+import json
+import sys
+import yaml
+
+
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        file = open(sys.argv[1], "r")
+        yaml.safe_load(file)
+        file.close()
+        print("YAML is VALID!")
+    else:
+        print(sys.argv[1] + "not found")
+else:
+    print("Usage: python check.yaml.py <file>")
